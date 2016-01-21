469ab8f5db6bce82d33fa5fcb241ac0b9ec87fe1
Chris Glover <c.d.glover@gmail.com>
2016-01-20 22:41:28 -0500

    Working version of simple binary.

---
+++ test/simple_binary_reader_writer.cpp
@@ -0,0 +1,58 @@
+// *****************************************************************************
+// 
+// reflect/test/binary_reader_writer.cpp
+//
+// Tests the functionality of the binary reader and writer.
+//
+// Copyright Chris Glover 2016
+//
+// Distributed under the Boost Software License, Version 1.0.
+// See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt
+//
+// ****************************************************************************
+#include <sstream>
+#include <cassert>
+
+#include "reflect/reflect_type.hpp"
+#include "reflect/serialize/simple_binary_writer.hpp"
+#include "reflect/serialize/simple_binary_reader.hpp"
+#include "test_user_types.hpp"
+
+int main()
+{
+	ns::Parent1 obj;
+
+	obj.f_.a_ = 1;
+	obj.f_.b_ = 2;
+	obj.s_.c_.push_back(3);
+	obj.s_.c_.push_back(4);
+	obj.s_.d_ = 5;
+	
+	std::stringstream archive;
+
+	reflect::serialize::simple_binary_writer<
+		std::stringstream
+	> binary_writer(archive);
+
+	binary_writer(obj);
+
+	obj.f_.a_ = 0;
+	obj.f_.b_ = 0;
+	obj.s_.c_.clear();
+	obj.s_.d_ = 0;
+
+	reflect::serialize::simple_binary_reader<
+		std::stringstream
+	> binary_reader(archive);
+
+	binary_reader(obj);
+
+	assert(obj.f_.a_ == 1);
+	assert(obj.f_.b_ == 2);
+	assert(obj.s_.c_[0] == 3);
+	assert(obj.s_.c_[1] == 4);
+	assert(obj.s_.d_ == 5);
+	
+	return 0;
+}
